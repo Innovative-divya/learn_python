b723243feb4bbd3d92c78e15f1fce2794729db0a
Divyanshi <142373656+Innovative-divya@users.noreply.github.com>
2023-12-29 23:27:34 +0530

    basic operators

---
+++ operators.py
@@ -0,0 +1,24 @@
+#Arithmetic Operators
+number = 1 + 2 * 3 / 4.0
+print(number)
+
+remainder = 11 % 3
+print(remainder)
+
+squared = 7 ** 2
+cubed = 2 ** 3
+print(squared)
+print(cubed)
+
+#Using Operators with Strings
+helloworld = "hello" + " " + "world"
+print(helloworld)
+
+lotsofhellos = "hello" * 10
+print(lotsofhellos)
+
+#Using Operators with Lists
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
